d1a9ee299ae7ed90be8e00ffc947b04266f5719c
landonkail <landon.kail@gmail.com>
2015-11-10 11:16:42 -0500

    add postgresql test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,14 @@ import java.sql.Connection;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+      testPost();
+    }
+
+    static void testPost () throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/hellodb");
 
         Statement stmt = conn.createStatement();
-        stmt.execute("CREATE TABLE IF NOT EXISTS players (name VARCHAR, health DOUBLE, score INT, is_alive BOOLEAN)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS players (name VARCHAR, health DECIMAL, score INT, is_alive BOOLEAN)");
         stmt.execute("INSERT INTO players VALUES ('Alice', 100, 10, true)");
         stmt.execute("INSERT INTO players VALUES ('Bob', 95, 10, true)");
         stmt.execute("UPDATE players SET health = 50 WHERE name = 'Alice'");
@@ -39,4 +43,36 @@ public class Main {
 
         conn.close();
     }
+
+   /* static void testH2 () {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS players (name VARCHAR, health DOUBLE, score INT, is_alive BOOLEAN)");
+        stmt.execute("INSERT INTO players VALUES ('Alice', 100, 10, true)");
+        stmt.execute("INSERT INTO players VALUES ('Bob', 95, 10, true)");
+        stmt.execute("UPDATE players SET health = 50 WHERE name = 'Alice'");
+        stmt.execute("DELETE FROM players WHERE name = 'Bob'");
+
+
+        String input = "Charlie";
+        PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO players VALUES (?, 100, 10, true)");
+        stmt2.setString(1, input);
+        stmt2.execute();
+
+        ResultSet results = stmt.executeQuery("SELECT * FROM players");
+        while (results.next()) {
+            String name = results.getString("name");
+            double health = results.getDouble("health");
+            int score = results.getInt("score");
+            boolean isAlive = results.getBoolean("is_alive");
+            System.out.println(String.format("%s %s %s %s", name, health, score, isAlive));
+        }
+
+        stmt.execute("DROP TABLE players");
+
+        conn.close();
+    }
+    */
+
 }
